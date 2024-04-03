0dc16076159c7cdef0c3fa8a91261fa704ea6769
Abbas <51442170+abbas-kr@users.noreply.github.com>
2024-04-03 10:56:09 +0330

    Update lang-mu.php

--- lang-mu.php
+++ lang-mu.php
@@ -10,10 +10,10 @@ add_filter( 'option_active_plugins', 'lang_disable_plugins_for_mobiles' );
 function lang_disable_plugins_for_mobiles( $plugins ) {
 
   if(strpos($_SERVER['REQUEST_URI'], '/en') === false) {
-    return $plugins; // for non-mobile device do nothing
+    return $plugins;
   }
 
-  $not_allowed = lang_non_mobile_plugins(); // get non allowed plugins
+  $not_allowed = lang_non_mobile_plugins();
 
   return array_values( array_diff( $plugins, $not_allowed ) );
 
